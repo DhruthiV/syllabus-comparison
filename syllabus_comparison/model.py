664d67d2fb4098845edc720c2b40eb29ea8d5629
DhruthiV <dhruthivenkatesh.pmcs.369@gmail.com>
2025-05-28 22:00:40 +0530

    combining two models for better embeddings

--- syllabus_comparison/model.py
+++ syllabus_comparison/model.py
@@ -1,4 +1,11 @@
 from sentence_transformers import SentenceTransformer
 
-def load_model():
-    return SentenceTransformer('all-MiniLM-L6-v2')
+# Loads 2 models
+
+# According to sbert documentation
+# The all-* models were trained on all available training data (more than 1 billion training pairs) and are designed as general purpose models. The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster and still offers good quality.
+def load_models():
+    model1 = SentenceTransformer("all-MiniLM-L6-v2")
+    model2 = SentenceTransformer("all-mpnet-base-v2")
+    return model1, model2
+
